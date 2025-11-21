f428a3089e1c6ca145c51cac116d98789ca78ea5
RinkuSoni7 <rinkusonissm12345@gmail.com>
2025-11-21 16:45:32 +0530

    Time: 79 ms (67.58%), Space: 56.1 MB (58.61%) - LeetHub

--- 1034-subarrays-with-k-different-integers/1034-subarrays-with-k-different-integers.cpp
+++ 1034-subarrays-with-k-different-integers/1034-subarrays-with-k-different-integers.cpp
@@ -1,43 +1,27 @@
 class Solution {
 public:
-    int subarraysWithKDistinct(vector<int>& nums, int k) {
-        int total=0;
-        unordered_map<int,int>freq;
-        int n=nums.size();
+    long long atMostK(vector<int>& nums, int K) {
+        unordered_map<int,int> freq;
+        long long ans = 0;
+        int start = 0;
 
-        int start=0, end=0, count=0;
-        while(end<n){
+        for (int end = 0; end < nums.size(); end++) {
             freq[nums[end]]++;
-            if(freq[nums[end]]==1)
-            count++;
 
-            while(count==k){
-                total+=n-end;
+            while (freq.size() > K) {
                 freq[nums[start]]--;
-                if(freq[nums[start]]==0)
-                count--;
+                if (freq[nums[start]] == 0)
+                    freq.erase(nums[start]);
                 start++;
             }
-            end++;
+            
+            ans += (end - start + 1);
         }
-        start=0, end=0, count=0;
-        freq.clear();
-        k++;
-        while(end<n){
-            freq[nums[end]]++;
-            if(freq[nums[end]]==1)
-            count++;
 
-            while(count==k){
-                total-=n-end;
-                freq[nums[start]]--;
-                if(freq[nums[start]]==0)
-                count--;
-                start++;
-            }
-            end++;
-        }
-        return total;
-        
+        return ans;
+    }
+
+    int subarraysWithKDistinct(vector<int>& nums, int K) {
+        return atMostK(nums, K) - atMostK(nums, K - 1);
     }
 };
