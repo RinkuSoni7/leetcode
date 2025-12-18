ccc0d26bae16deb4d7be5f6b123dcee094639704
RinkuSoni7 <rinkusonissm12345@gmail.com>
2025-12-19 04:34:46 +0530

    Time: 2 ms (12.91%), Space: 12.1 MB (31.63%) - LeetHub

--- 0014-longest-common-prefix/0014-longest-common-prefix.cpp
+++ 0014-longest-common-prefix/0014-longest-common-prefix.cpp
@@ -1,66 +1,20 @@
-class TrieNode {
-public:
-    TrieNode* children[26];
-    bool isTerminal;
-    int childCount;
-
-    TrieNode() {
-        for (int i = 0; i < 26; i++)
-            children[i] = nullptr;
-        isTerminal = false;
-        childCount = 0;
-    }
-};
-
-class Trie {
-public:
-    TrieNode* root;
-
-    Trie() {
-        root = new TrieNode();
-    }
-
-    void insert(const string& word) {
-        TrieNode* curr = root;
-
-        for (char ch : word) {
-            int index = ch - 'a';
-            if (curr->children[index] == nullptr) {
-                curr->children[index] = new TrieNode();
-                curr->childCount++;
-            }
-            curr = curr->children[index];
-        }
-        curr->isTerminal = true;
-    }
-
-    string getLCP() {
-        string ans = "";
-        TrieNode* curr = root;
-
-        while (curr && curr->childCount == 1 && curr->isTerminal == false) {
-            for (int i = 0; i < 26; i++) {
-                if (curr->children[i]) {
-                    curr = curr->children[i];
-                    ans.push_back('a' + i);
-                    break;
-                }
-            }
-        }
-        return ans;
-    }
-};
-
 class Solution {
 public:
     string longestCommonPrefix(vector<string>& strs) {
-        if (strs.empty()) return "";
+        int n = strs.size();
+        if (n == 0) return "";
 
-        Trie trie;
-        for (string& s : strs) {
-            trie.insert(s);
+        sort(strs.begin(), strs.end());
+        string a = strs[0];
+        string b = strs[n - 1];
+        string ans = "";
+
+        for (int i = 0; i < min(a.size(), b.size()); i++) {
+            if (a[i] == b[i])
+                ans += a[i];
+            else
+                break;
         }
-
-        return trie.getLCP();
+        return ans;
     }
 };
