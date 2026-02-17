fed1863a23ef59c8c2b01e25e2f6cf7b5d4bc1d3
RinkuSoni7 <rinkusonissm12345@gmail.com>
2026-02-17 13:57:16 +0530

    Time: 0 ms (100%), Space: 14.3 MB (52.66%) - LeetHub

--- 0349-intersection-of-two-arrays/0349-intersection-of-two-arrays.cpp
+++ 0349-intersection-of-two-arrays/0349-intersection-of-two-arrays.cpp
@@ -1,21 +1,22 @@
 class Solution {
 public:
     vector<int> intersection(vector<int>& nums1, vector<int>& nums2) {
-        vector<int>result;
-        for(int i=0; i<nums1.size(); i++){
-        // vector<int>result;
+       unordered_map<int,int>mp;
+       vector<int>result;
+       
+       for(int i=0; i<nums1.size(); i++){
+        mp[nums1[i]]++;
+       }
+       
 
-            for(int j=0; j<nums2.size(); j++){
-                if(nums1[i]==nums2[j]){
-                    result.push_back(nums1[i]);
-                    break;
-                }
-            }
+       for(auto &num : nums2){
+        if(mp.find(num)!=mp.end()){
+            result.push_back(num);
+            mp.erase(num);
         }
-        sort(result.begin(), result.end());
-        result.erase(unique(result.begin(), result.end()), result.end());
-
-        return result;
+        
+       }
+       return result;
 
     }
 };
