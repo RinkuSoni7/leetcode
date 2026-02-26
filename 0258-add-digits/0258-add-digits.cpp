253e837d95a395c916249cb01941035d1bd87393
RinkuSoni7 <rinkusonissm12345@gmail.com>
2026-02-26 12:23:02 +0530

    Time: 0 ms (100%), Space: 8.7 MB (7.17%) - LeetHub

---
+++ 0258-add-digits/0258-add-digits.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    int addDigits(int num) {
+    
+    
+    while(num>9){
+int ans=0;
+        while(num>0){
+            int rem=num%10;
+            
+            ans+=rem;
+            num/=10;
+        }
+        num=ans;
+
+    }
+return num;
+        
+    }
+};
