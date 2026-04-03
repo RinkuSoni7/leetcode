df715bb19f86c2ebcab071d1d007ca7eb0c0abb5
RinkuSoni7 <rinkusonissm12345@gmail.com>
2026-04-04 01:39:55 +0530

    Time: 3 ms (9.91%), Space: 8.8 MB (66.81%) - LeetHub

--- 0020-valid-parentheses/0020-valid-parentheses.cpp
+++ 0020-valid-parentheses/0020-valid-parentheses.cpp
@@ -2,21 +2,27 @@ class Solution {
 public:
     bool isValid(string s) {
         stack<char>st;
-        for(char c :s){
-            if(c=='(' || c=='{' || c=='['){
-                st.push(c);
-            }else{
-                if (st.empty()) return false;
-                char top = st.top();
-                st.pop();
-
-                if ((c == ')' && top != '(') ||
-                    (c == '}' && top != '{') ||
-                    (c == ']' && top != '[')) {
-                    return false;
+        for(char ch : s){
+            if(ch =='(' || ch=='{' || ch=='['){
+                st.push(ch);
             }
+
+            else if(st.empty()){
+                return false;
+
+            }
+               else if(st.top()=='(' && ch==')' ||
+                st.top()=='{' && ch=='}' ||
+                st.top()=='[' && ch==']'){
+                    st.pop();
+                }
+                else{
+                    return false;
+                }
+            
+
         }
-        }
+
         return st.empty();
 
 
