2e8e14f61c09dd42e9a75726e831aebcfd4a5b5f
RinkuSoni7 <rinkusonissm12345@gmail.com>
2026-04-19 11:59:03 +0530

    Time: 0 ms (100%), Space: 17.6 MB (49.25%) - LeetHub

--- 0746-min-cost-climbing-stairs/0746-min-cost-climbing-stairs.cpp
+++ 0746-min-cost-climbing-stairs/0746-min-cost-climbing-stairs.cpp
@@ -8,6 +8,6 @@ public:
         for(int i=2; i<n; i++){
             cost[i]=cost[i]+min(cost[i-1],cost[i-2]);
         }
-        return min(cost[n-1],cost[n-2]);
+        return 0+min(cost[n-1],cost[n-2]);
     }
 };
