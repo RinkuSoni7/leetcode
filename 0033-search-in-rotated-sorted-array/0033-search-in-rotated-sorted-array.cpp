1a4c40bbbf49d58c6176fe031fcdf386d86d475d
RinkuSoni7 <rinkusonissm12345@gmail.com>
2026-04-27 23:21:17 +0530

    Time: 0 ms (100%), Space: 15.2 MB (34.49%) - LeetHub

---
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.cpp
@@ -0,0 +1,44 @@
+class Solution {
+public:
+
+int getpivot(vector<int>& nums,int n){
+    int s=0,e=n-1;
+    while(s<e){
+        int mid=s+(e-s)/2;
+        if(nums[mid]>nums[e]){
+            s=mid+1;
+        }else{
+            e=mid;
+        }
+    }
+    return s;
+}
+
+int binary(vector<int>& nums,int s,int e,int k){
+    while(s<=e){
+        int mid=s+(e-s)/2;
+        if(nums[mid]==k){
+            return mid;
+        }else if(nums[mid]>k){
+            e=mid-1;
+        }else{
+            s=mid+1;
+        }
+    }
+    return -1;
+}
+
+
+    int search(vector<int>& nums, int target) {
+
+        int n=nums.size();
+        int k=target;
+        int pivot=getpivot(nums,n);
+
+        if(k>=nums[pivot] && k<=nums[n-1]){
+            return binary(nums,pivot,n-1,k);
+        }else{
+            return binary(nums,0,pivot-1,k);
+        }
+    }
+};
