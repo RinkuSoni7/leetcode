e34873d2d69116e52b8bd9d73a0a57743d85f97c
RinkuSoni7 <rinkusonissm12345@gmail.com>
2026-05-05 16:15:45 +0530

    Time: 11 ms (96.43%), Space: 95.7 MB (80.75%) - LeetHub

---
+++ 2870-minimum-number-of-operations-to-make-array-empty/2870-minimum-number-of-operations-to-make-array-empty.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    int minOperations(vector<int>& nums) {
+        unordered_map<int,int>mp;
+        for(int i=0; i<nums.size(); i++){
+            mp[nums[i]]++;
+        }
+        int result=0;
+
+        for(auto &it:mp){
+            int freq=it.second;
+            if(freq==1){
+                return -1;
+            }
+
+            result+=ceil((double)freq/3);
+
+        }
+
+        return result;
+    }
+};
