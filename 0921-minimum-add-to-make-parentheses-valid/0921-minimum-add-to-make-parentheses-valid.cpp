3e00f3f69e39a6b82c5b09933273f705a9d741fb
RinkuSoni7 <rinkusonissm12345@gmail.com>
2026-06-24 16:21:40 +0530

    Time: 0 ms (100%), Space: 8.7 MB (12.66%) - LeetHub

---
+++ 0921-minimum-add-to-make-parentheses-valid/0921-minimum-add-to-make-parentheses-valid.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    int minAddToMakeValid(string s) {
+        int n=s.length();
+        int count=0;
+        stack<char>st;
+        for(char &ch : s){
+            if(ch=='('){
+                st.push(ch);
+                count++;
+            }else{
+                if(st.empty()){
+                    count++;
+                    st.push(ch);
+
+                }else if(st.top()!='('){
+                    count++;
+                    st.push(ch);
+                }else{
+                    count--;
+                    st.pop();
+                }
+            }
+        }
+        return count;
+    }
+};
