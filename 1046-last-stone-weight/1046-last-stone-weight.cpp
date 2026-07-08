678a925e9e6636c5f7977103242dc51035086900
RinkuSoni7 <rinkusonissm12345@gmail.com>
2026-07-08 15:50:17 +0530

    Time: 0 ms (100%), Space: 9.9 MB (51.02%) - LeetHub

--- 1046-last-stone-weight/1046-last-stone-weight.cpp
+++ 1046-last-stone-weight/1046-last-stone-weight.cpp
@@ -1,25 +1,26 @@
 class Solution {
 public:
     int lastStoneWeight(vector<int>& stones) {
+        int n=stones.size();
         priority_queue<int>pq;
-        for(int i=0; i<stones.size(); i++){
+        for(int i=0; i<n; i++){
             pq.push(stones[i]);
         }
-        
+
         while(pq.size()>1){
             int first=pq.top();
             pq.pop();
             int second=pq.top();
             pq.pop();
+
             if(first!=second){
-            int num=first-second;
-            pq.push(num);
-            }
-            if(first==second){
+                int num=first-second;
+                pq.push(num);
+            }else{
                 pq.push(0);
             }
         }
-        // return pq.empty() ? 0 : pq.top();
         return pq.top();
+        
     }
 };
