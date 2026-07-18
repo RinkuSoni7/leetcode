6e48b5ccf1e7c1f06e5ad34409f02b1d7862b9a1
RinkuSoni7 <rinkusonissm12345@gmail.com>
2026-07-18 18:12:01 +0530

    Time: 49 ms (7.68%), Space: 85.7 MB (44.72%) - LeetHub

--- 0084-largest-rectangle-in-histogram/0084-largest-rectangle-in-histogram.cpp
+++ 0084-largest-rectangle-in-histogram/0084-largest-rectangle-in-histogram.cpp
@@ -1,53 +1,60 @@
 class Solution {
-    private:
-    vector<int> nextsmaller(vector<int> arr,int n){
-        stack<int> st;
-        st.push(-1);
-      vector<int> ans(n);
-    for(int i=n-1; i>=0; i--){
-        int curr=arr[i];
-        while(st.top() !=-1 && arr[st.top()]>=curr){
-            st.pop();
-        }
-        
-        ans[i]=st.top();
-        st.push(i);
-        }
-        return ans;
-    }
-
-    vector<int> nextprev(vector<int> arr,int n){
-        stack<int> st;
-        st.push(-1);
-      vector<int> ans(n);
-    for(int i=0; i<n; i++){
-        int curr=arr[i];
-        while(st.top() !=-1 && arr[st.top()]>=curr){
-            st.pop();
-        }
-        
-        ans[i]=st.top();
-        st.push(i);
-        }
-        return ans;
-    }
 public:
+vector<int>prevsmaller(vector<int>& arr,int n){
+    stack<int>st;
+    st.push(-1);
+    vector<int>ans(n);
+
+    for(int i=0; i<n; i++){
+        while(st.top()!=-1 && arr[st.top()]>=arr[i]){
+            st.pop();
+        }
+        ans[i]=st.top();
+        st.push(i);
+    }
+    return ans;
+}
+
+vector<int>nextsmaller(vector<int>& arr,int n){
+    stack<int>st;
+    st.push(-1);
+    vector<int>ans(n);
+
+    for(int i=n-1; i>=0; i--){
+        while(st.top()!=-1 && arr[st.top()]>=arr[i]){
+            st.pop();
+        }
+        ans[i]=st.top();
+        st.push(i);
+    }
+    return ans;
+}
+
+
+
     int largestRectangleArea(vector<int>& heights) {
-        // int area=0
         int n=heights.size();
-        vector<int> next(n);
-        next=nextsmaller(heights,n);
-        vector<int> prev(n);
-        prev=nextprev(heights,n);
-        int area=INT_MIN;
+
+        vector<int>prev=prevsmaller(heights,n);
+            vector<int>next=nextsmaller(heights,n);
+
+
+        int area=0;
+        int ans=0;
         for(int i=0; i<n; i++){
-            int l=heights[i];
-            // int b=next[i]-prev[i]-1;
+
+            
+            int h=heights[i];
+
             if(next[i]==-1){
                 next[i]=n;
+
             }
-            int b=next[i]-prev[i]-1;
-            int newarea=l*b;
+
+            int width=next[i]-prev[i]-1;
+
+            int newarea=width*h;
+
             area=max(area,newarea);
         }
         return area;
