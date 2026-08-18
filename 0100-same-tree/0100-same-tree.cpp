b524cd7ab73984e76351d0177e65c4e22c442d33
RinkuSoni7 <rinkusonissm12345@gmail.com>
2026-08-18 09:37:19 +0530

    Time: 0 ms (100%), Space: 13 MB (13.4%) - LeetHub

--- 0100-same-tree/0100-same-tree.cpp
+++ 0100-same-tree/0100-same-tree.cpp
@@ -12,22 +12,44 @@
 class Solution {
 public:
     bool isSameTree(TreeNode* p, TreeNode* q) {
-        if(p==NULL && q==NULL){
+
+        // Dono NULL hain
+        if (p == NULL && q == NULL) {
             return true;
         }
-        if((p==NULL && q!=NULL) ||(p!=NULL && q==NULL)){
+
+        // Ek NULL hai aur doosra NULL nahi hai
+        if ((p == NULL && q != NULL) ||
+            (p != NULL && q == NULL)) {
             return false;
         }
 
-
-        if(p->val!=q->val){
+       
+        if (p->val != q->val) {
             return false;
         }
 
-        return isSameTree(p->left,q->left)&& isSameTree(p->right,q->right);
+        if ((p->left != NULL && q->left == NULL) ||
+            (p->left == NULL && q->left != NULL)) {
+            return false;
+        }
 
         
+        if ((p->right != NULL && q->right == NULL) ||
+            (p->right == NULL && q->right != NULL)) {
+            return false;
+        }
 
+        
+        if (!isSameTree(p->left, q->left)) {
+            return false;
+        }
 
+        
+        if (!isSameTree(p->right, q->right)) {
+            return false;
+        }
+
+        return true;
     }
 };
