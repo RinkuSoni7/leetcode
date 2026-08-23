d7c9976c845b5760dc78c250be11ebc1b3511114
RinkuSoni7 <rinkusonissm12345@gmail.com>
2026-08-23 20:08:52 +0530

    Time: 0 ms (100%), Space: 16.4 MB (46.33%) - LeetHub

--- 1008-construct-binary-search-tree-from-preorder-traversal/1008-construct-binary-search-tree-from-preorder-traversal.cpp
+++ 1008-construct-binary-search-tree-from-preorder-traversal/1008-construct-binary-search-tree-from-preorder-traversal.cpp
@@ -1,34 +1,34 @@
-/**
- * Definition for a binary tree node.
- * struct TreeNode {
- *     int val;
- *     TreeNode *left;
- *     TreeNode *right;
- *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
- *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
- * };
- */
 class Solution {
 public:
-TreeNode*solve(vector<int>& preorder,int mini,int max,int &i){
-    if(i>=preorder.size()) return NULL;
 
-    if(preorder[i] < mini || preorder[i] > max) return NULL;
-    TreeNode *root=new TreeNode(preorder[i++]);
-    root->left=solve(preorder,mini,root->val,i);
-    root->right=solve(preorder,root->val,max,i);
-    return root;
+    TreeNode* insert(TreeNode* root, int val) {
 
+      
+        if (root == NULL) {
+            return new TreeNode(val);
+        }
 
+   
+        if (val < root->val) {
+            root->left = insert(root->left, val);
+        }
 
-}
+        
+        else {
+            root->right = insert(root->right, val);
+        }
 
-    TreeNode *bstFromPreorder(vector<int>& preorder) {
-        int mini=INT_MIN;
-        int max=INT_MAX;
-        int i=0;
-        return solve(preorder,mini,max,i);
+        return root;
+    }
 
+    TreeNode* bstFromPreorder(vector<int>& preorder) {
+
+        TreeNode* root = NULL;
+
+        for (int i = 0; i < preorder.size(); i++) {
+            root = insert(root, preorder[i]);
+        }
+
+        return root;
     }
 };
