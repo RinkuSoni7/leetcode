6fd51ea500726220dd0473142533b9cd35eed9a9
RinkuSoni7 <rinkusonissm12345@gmail.com>
2026-09-06 22:05:17 +0530

    Time: 9 ms (48.64%), Space: 14.7 MB (32.49%) - LeetHub

---
+++ 1249-minimum-remove-to-make-valid-parentheses/1249-minimum-remove-to-make-valid-parentheses.cpp
@@ -0,0 +1,41 @@
+class Solution {
+public:
+    string minRemoveToMakeValid(string s) {
+        int n = s.length();
+
+        stack<int> st;
+        unordered_set<int> idx;
+
+        for(int i = 0; i < n; i++) {
+
+            if(s[i] == '(') {
+                st.push(i);
+            }
+            else if(s[i] == ')') {
+
+                if(st.empty()) {
+                    idx.insert(i);
+                }
+                else {
+                    st.pop();
+                }
+            }
+        }
+
+        // Unmatched '('
+        while(!st.empty()) {
+            idx.insert(st.top());
+            st.pop();
+        }
+
+        string ans = "";
+
+        for(int i = 0; i < n; i++) {
+            if(idx.find(i) == idx.end()) {
+                ans.push_back(s[i]);
+            }
+        }
+
+        return ans;
+    }
+};
