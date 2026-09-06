b9df80f21f2ae0ebc59cdf4071aa95f54e36f826
RinkuSoni7 <rinkusonissm12345@gmail.com>
2026-09-06 15:24:09 +0530

    Time: 90 ms (16.18%), Space: 193.4 MB (75.41%) - LeetHub

--- 0907-sum-of-subarray-minimums/0907-sum-of-subarray-minimums.cpp
+++ 0907-sum-of-subarray-minimums/0907-sum-of-subarray-minimums.cpp
@@ -1,62 +1,60 @@
 class Solution {
 public:
-vector<int>getnsl(vector<int>& arr,int n){
+vector<int>prevsmaller(vector<int>&arr,int n){
     stack<int>st;
     st.push(-1);
     vector<int>ans(n);
     for(int i=0; i<n; i++){
-        while(st.top()!=-1 && arr[st.top()]> arr[i]){
+        while(st.top()!=-1 && arr[st.top()]>= arr[i]){
             st.pop();
         }
         ans[i]=st.top();
         st.push(i);
-
     }
     return ans;
 }
 
-vector<int>getnsr(vector<int>& arr,int n){
+vector<int>nextsmaller(vector<int>&arr,int n){
     stack<int>st;
-    st.push(-1);
+    // st.push(-1);
     vector<int>ans(n);
-
     for(int i=n-1; i>=0; i--){
-        while(st.top()!=-1 && arr[st.top()]>= arr[i]){
+        while(!st.empty() && arr[st.top()]> arr[i]){
             st.pop();
         }
-        ans[i] = (st.top() == -1) ? n : st.top();
+        if(st.empty()){
+            ans[i]=n;
+        }else{
+            ans[i]=st.top();
+        }
+        
         st.push(i);
-
     }
     return ans;
 }
-
-
-
-
-
     int sumSubarrayMins(vector<int>& arr) {
         int n=arr.size();
-        vector<int>nsl=getnsl(arr,n);
-        vector<int>nsr=getnsr(arr,n);
+        vector<int>prev=prevsmaller(arr,n);
+        vector<int>next=nextsmaller(arr,n);
+
         int M=1e9+7;
 
         long long sum=0;
-
         for(int i=0; i<n; i++){
-            long long ls =i- nsl[i];
-            long long rs =nsr[i]-i;
+            long long ls=i-prev[i];
+            long long rs=next[i]-i;
 
-            long long totalways=ls*rs;
+            long long total=ls*rs;
 
-            long long totalsum=(arr[i]*totalways)%M;
+            long long totalsum=(arr[i]*total)%M;
+
+
 
             sum=(sum+totalsum)%M;
 
+
         }
         return sum;
-
-
 
     }
 };
