ec0367da1738af576b37d4376c1da7a8722174e0
RinkuSoni7 <rinkusonissm12345@gmail.com>
2026-09-08 11:58:13 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0044-wildcard-matching/0044-wildcard-matching.cpp
+++ 0044-wildcard-matching/0044-wildcard-matching.cpp
@@ -1,11 +1,11 @@
 class Solution {
 public:
-bool solve(string &s, string& p,int i,int j,vector<vector<int>>&dp){
+bool solve(string &s,string &p,int i,int j,vector<vector<int>> &dp){
     if(i<0 && j<0) return true;
     if(i>=0 && j<0) return false;
 
     if(i<0 && j>=0){
-        for(int k=0; k<=j; k++){
+        for(int k=0; k<j; k++){
             if(p[k]!='*'){
                 return false;
             }
@@ -16,15 +16,15 @@ bool solve(string &s, string& p,int i,int j,vector<vector<int>>&dp){
     if(dp[i][j]!=-1){
         return dp[i][j];
     }
-    
-    if(s[i]==p[j] || p[j]=='?') {
-     return dp[i][j]=solve(s,p,i-1,j-1,dp);
-    }else if(p[j]=='*'){
-        return dp[i][j]=solve(s,p,i,j-1,dp) || solve(s,p,i-1,j,dp);
-    }
-    else{
-        return dp[i][j]=false;
-    }
+
+if(s[i]==p[j] || p[j]=='?'){
+    return dp[i][j]=solve(s,p,i-1,j-1,dp);
+}else if(p[j]=='*'){
+    return dp[i][j]=solve(s,p,i-1,j,dp) || solve(s,p,i,j-1,dp);
+}else{
+
+    return dp[i][j]=false;
+}
 
 
 }
