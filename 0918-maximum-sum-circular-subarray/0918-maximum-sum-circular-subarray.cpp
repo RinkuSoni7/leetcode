f94b4930736fe74366ad1ee13fe61fa5858aa6dc
RinkuSoni7 <rinkusonissm12345@gmail.com>
2026-09-24 21:35:54 +0530

    Time: 0 ms (100%), Space: 43.5 MB (77.28%) - LeetHub

--- 0918-maximum-sum-circular-subarray/0918-maximum-sum-circular-subarray.cpp
+++ 0918-maximum-sum-circular-subarray/0918-maximum-sum-circular-subarray.cpp
@@ -1,48 +1,49 @@
 class Solution {
 public:
-int kadansmaxsum(vector<int>& nums,int n){
+
+int kadansmin(vector<int>&nums,int n){
     int sum=0;
-    int ans=INT_MIN;
+    int mini=INT_MAX;
     for(int i=0; i<n; i++){
         sum+=nums[i];
-        ans=max(ans,sum);
-
-        if(sum<0) sum=0;
+        mini=min(sum,mini);
+        if(sum>0){
+            sum=0;
+        }
     }
-    return ans;
+    return mini;
 }
 
-
-int kadansminsum(vector<int>& nums,int n){
+int kadansmax(vector<int>&nums,int n){
     int sum=0;
-    int ans=INT_MAX;
+    int maxi=INT_MIN;;
     for(int i=0; i<n; i++){
         sum+=nums[i];
-        ans=min(ans,sum);
-
-        if(sum>0) sum=0;
+        maxi=max(sum,maxi);
+        if(sum<0){
+            sum=0;
+        }
     }
-    return ans;
+    return maxi;
 }
-    
+
     int maxSubarraySumCircular(vector<int>& nums) {
         int n=nums.size();
-
         int sum=0;
-
         for(int i=0; i<n; i++){
             sum+=nums[i];
         }
 
-        int maxisum=kadansmaxsum(nums,n);
-        int minisum=kadansminsum(nums,n);
+        int maxsum=kadansmax(nums,n);
+        int minsum=kadansmin(nums,n);
 
-        int circular_sum=sum-minisum;
 
-        if(maxisum>0){
-            return max(maxisum,circular_sum);
+        int circularsum=sum-minsum;
+
+        if(maxsum>0){
+           return max(maxsum,circularsum);
         }else{
-            return maxisum;
+            return maxsum;
         }
 
         
