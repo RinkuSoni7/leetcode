145d16adec16003cdbfccdfb3ce3593372cf5497
RinkuSoni7 <rinkusonissm12345@gmail.com>
2026-09-25 01:17:53 +0530

    Time: 1056 ms (5%), Space: 60.6 MB (7.02%) - LeetHub

---
+++ 0567-permutation-in-string/0567-permutation-in-string.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    bool checkInclusion(string s1, string s2) {
+        int n=s1.length();
+        int m=s2.length();
+
+        if(n>m){
+            return false;
+        }
+
+        sort(begin(s1),end(s1));
+
+        for(int i=0; i<=m-n; i++){
+            string temp=s2.substr(i,n);
+
+            sort(begin(temp),end(temp));
+
+            if(temp==s1){
+                return true;
+            }
+        }
+return false;
+        
+    }
+};
